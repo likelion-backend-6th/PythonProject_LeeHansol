b85bb90612de2ad7d9fea19f0a2e2c1cba816d39
Hansol Lee <gksthf996@naver.com>
2023-07-05 21:12:03 +0900

    시스템 메인 메뉴 구성

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-..
+library_system = {
+    '1': {'option':'도서 정보 조회'},
+    '2': {'option':'도서 대출'},
+    '3': {'option':'도서 반납'},
+    '4': {'option':'대출 정보 조회'},
+    '5': {'option':'종료'},
+}
+
+def print_system():
+    print('\n-------------------------------\n '
+          '도서관 관리 시스템 메인 메뉴 \n'
+          '-------------------------------')
+    for id, system in library_system.items():
+        print(f"{id}. {system['option']}")
+
+
+
+
