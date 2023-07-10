85bc96117b6511803622b2ca0f498e13191e1a03
Hansol Lee <gksthf996@naver.com>
2023-07-10 22:03:52 +0900

    메뉴 별 기능 구현(하고싶었지만 안됨)

--- main.py
+++ main.py
@@ -1,34 +1,73 @@
+import psycopg2
+
+conn = psycopg2.connect(
+    host='localhost',
+    port='5433',
+    dbname='postgres',
+    user='postgres',
+    password='jkhldh940411'
+)
+
+cursor = conn.cursor()
+
+
 library_system = {
     '1': {'option': '도서 정보 조회'},
     '2': {'option': '도서 대출'},
     '3': {'option': '도서 반납'},
-    '4': {'option': '대출 정보 조회'},
-    '5': {'option': '종료'},
+    '4': {'option': '종료'},
 }
 
 
 def print_system():
     print('\n-------------------------------\n '
-          '도서관 관리 시스템 메인 메뉴 \n'
+          '    도서관 관리 시스템 메인 메뉴\n'
           '-------------------------------')
     for id, system in library_system.items():
         print(f"{id}. {system['option']}")
 
 
 def book_info():
-    pass
+    while True:
+        print('\n-------------------------------\n'
+            '        도서 정보 조회\n'
+            '-------------------------------')
+        choice = input('\n조회 할 도서의 id를 입력하세요(이전메뉴:exit): ')
+        cursor.execute("SELECT id FROM library;")
+        result = cursor.fetchall()
+        print(result)
+
+        if choice == 'exit':
+            print_system()
 
 
 def book_loan():
-    pass
+    while True:
+        print('\n-------------------------------\n'
+            '      도서 대출 서브 메뉴\n'
+            '-------------------------------')
+        choice = input('\n대출 할 도서의 id를 입력하세요(이전메뉴:exit): ')
+        cursor.execute("UPDATE library SET is_available = '0' WHERE is_available = '1';")
+        cursor.commit()
+        print("대출이 완료 되었습니다.")
+
+        if choice == 'exit':
+            print_system()
+
 
 
 def book_return():
-    pass
+    while True:
+        print('\n-------------------------------\n'
+            '      도서 반납 서브 메뉴\n'
+            '-------------------------------')
+        choice = input('\n반납 할 도서의 id를 입력하세요(이전메뉴:exit): ')
+        cursor.execute("UPDATE library SET is_available = '1' WHERE is_available = '0';")
+        cursor.commit()
+        print("반납이 완료 되었습니다.")
 
-
-def loan_info():
-    pass
+        if choice == 'exit':
+            print_system()
 
 
 while True:
@@ -37,18 +76,24 @@ while True:
                    '\n사용할 시스템 메뉴를 선택하세요:')
     if choice == '1':
         book_info()
+        break
 
     elif choice == '2':
         book_loan()
+        break
+
 
     elif choice == '3':
         book_return()
+        break
+
 
     elif choice == '4':
-        loan_info()
-
-    elif choice == '5':
         print("시스템을 종료합니다. 이용해주셔서 감사합니다.")
         break
+
     else:
         print("\n잘못된 선택입니다. 다시 선택해주세요.")
+
+cursor.close()
+conn.close()
