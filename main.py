219cd2dba65d85ecdaa708801e444017fcef2a38
Hansol Lee <gksthf996@naver.com>
2023-07-11 22:26:36 +0900

    메뉴 별 기능 구현 수정(한 것 같은데 잘 모르겠어요)

--- main.py
+++ main.py
@@ -32,13 +32,15 @@ def book_info():
         print('\n-------------------------------\n'
             '        도서 정보 조회\n'
             '-------------------------------')
-        choice = input('\n조회 할 도서의 id를 입력하세요(이전메뉴:exit): ')
-        cursor.execute("SELECT id FROM library;")
+        info_input = input('\n조회 할 도서의 id를 입력하세요(이전메뉴:exit): ')
+        cursor.execute("SELECT * FROM books WHERE book_id = info_input;")
         result = cursor.fetchall()
         print(result)
 
-        if choice == 'exit':
-            print_system()
+        if info_input == 'exit':
+            break
+
+    choice_system()
 
 
 def book_loan():
@@ -46,13 +48,14 @@ def book_loan():
         print('\n-------------------------------\n'
             '      도서 대출 서브 메뉴\n'
             '-------------------------------')
-        choice = input('\n대출 할 도서의 id를 입력하세요(이전메뉴:exit): ')
-        cursor.execute("UPDATE library SET is_available = '0' WHERE is_available = '1';")
-        cursor.commit()
+        loan_input = input('\n대출 할 도서의 id를 입력하세요(이전메뉴:exit): ')
+        cursor.execute("UPDATE books SET is_available = FALSE WHERE is_available = TRUE;")
         print("대출이 완료 되었습니다.")
 
-        if choice == 'exit':
-            print_system()
+        if loan_input == 'exit':
+            break
+
+    choice_system()
 
 
 
@@ -61,39 +64,42 @@ def book_return():
         print('\n-------------------------------\n'
             '      도서 반납 서브 메뉴\n'
             '-------------------------------')
-        choice = input('\n반납 할 도서의 id를 입력하세요(이전메뉴:exit): ')
-        cursor.execute("UPDATE library SET is_available = '1' WHERE is_available = '0';")
-        cursor.commit()
+        return_input = input('\n반납 할 도서의 id를 입력하세요(이전메뉴:exit): ')
+        cursor.execute("UPDATE books SET is_available = TRUE WHERE is_available = FALSE;")
         print("반납이 완료 되었습니다.")
 
-        if choice == 'exit':
-            print_system()
+        if return_input == 'exit':
+            break
+
+    choice_system()
+
+def choice_system():
+    while True:
+        print_system()
+        choice = input('-------------------------------'
+                    '\n사용할 시스템 메뉴를 선택하세요:')
+        if choice == '1':
+            book_info()
+            break
+
+        elif choice == '2':
+            book_loan()
+            break
 
 
-while True:
-    print_system()
-    choice = input('-------------------------------'
-                   '\n사용할 시스템 메뉴를 선택하세요:')
-    if choice == '1':
-        book_info()
-        break
-
-    elif choice == '2':
-        book_loan()
-        break
+        elif choice == '3':
+            book_return()
+            break
 
 
-    elif choice == '3':
-        book_return()
-        break
+        elif choice == '4':
+            print("시스템을 종료합니다. 이용해주셔서 감사합니다.")
+            break
 
+        else:
+            print("\n잘못된 선택입니다. 다시 선택해주세요.")
 
-    elif choice == '4':
-        print("시스템을 종료합니다. 이용해주셔서 감사합니다.")
-        break
-
-    else:
-        print("\n잘못된 선택입니다. 다시 선택해주세요.")
+choice_system()
 
 cursor.close()
 conn.close()
